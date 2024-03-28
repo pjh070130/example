61eecfd52b23b7db1c99bab010845b31f906df6e
pjh070130 <kko20_s23_10206@gclass.ice.go.kr>
2024-03-28 20:55:32 +0900

    [ADD]order

--- demo/src/main/java/order/Order.java
+++ demo/src/main/java/order/Order.java
@@ -1,2 +1,9 @@
-package order;public class Order {
+package order;
+
+public class Order {
+    int orderId;
+    String name;
+    public void printOrderId(){
+        System.out,println(orderId);
+    }
 }
